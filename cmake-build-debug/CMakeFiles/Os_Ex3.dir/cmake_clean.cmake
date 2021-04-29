37fedfb92cda381869cc42ddd30424623cc0c47d
AvrahamBi <avrahambarilan@gmail.com>
2021-04-29 17:15:45 +0300

    first commit

---
+++ cmake-build-debug/CMakeFiles/Os_Ex3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Os_Ex3.dir/main.c.o"
+  "Os_Ex3"
+  "Os_Ex3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Os_Ex3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
